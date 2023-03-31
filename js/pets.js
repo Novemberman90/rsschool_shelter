408300f676b244086ff8f2dc235eb043dcefd0f2
Alexey <alexledovit@gmail.com>
2023-03-31 04:16:38 +0300

    init: js and pets page budrer menu

---
+++ js/pets.js
@@ -0,0 +1,11 @@
+// burger 
+
+const iconMenu = document.querySelector('.menu_icon');
+if(iconMenu) {
+    const menuBody = document.querySelector('.menu_body');
+    iconMenu.addEventListener("click", function (e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+
+}
