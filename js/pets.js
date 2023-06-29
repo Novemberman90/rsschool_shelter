060cc047e8bcdad745cc6e4845603d931475a6b3
Alexey <alexledovit@gmail.com>
2023-06-30 02:14:39 +0300

    fix: fixed const

--- js/pets.js
+++ js/pets.js
@@ -1,11 +1,12 @@
 // burger 
 
-const iconMenu = document.querySelector(".menu_icon");
+const iconMenu = document.querySelector('.menu_icon');
 if(iconMenu) {
-    const menuBody = document.querySelector(".menu_body");
-    iconMenu.addEventListener("click", function (e) {
-        iconMenu.classList.toggle("_active");
-        menuBody.classList.toggle("_active");
+    const menuBody = document.querySelector('.menu_body');
+    iconMenu.addEventListener('click', function (e) {
+      document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
     });
 }
 
